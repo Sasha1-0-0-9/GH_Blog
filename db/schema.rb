6ff25691f15ca2b153c3e88e913c807def34fdcb
Sasha <sashapysmenny@gmail.com>
2021-01-07 22:20:33 +0200

    db fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_103112) do
+ActiveRecord::Schema.define(version: 2021_01_07_201813) do
 
   create_table "author_comment_votes", force: :cascade do |t|
     t.integer "comment_id"
