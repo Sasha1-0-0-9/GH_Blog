c0bbd23080c9922d0ac3733f82cab5cfee05477c
Sasha <sashapysmenny@gmail.com>
2021-01-08 00:00:51 +0200

    test

--- db/migrate/20210107215307_create_author_comment_vote.rb
+++ db/migrate/20210107215307_create_author_comment_vote.rb
@@ -1,8 +1,6 @@
 class CreateAuthorCommentVote < ActiveRecord::Migration[6.1]
   def change
     create_table :author_comment_votes do |t|
-      t.references :author
-      t.references :comment
       t.boolean :positive
       t.timestamps
     end
